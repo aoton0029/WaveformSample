ee60e78a6444f60f4b0b1162b9d290cd2042f08c
aoton0029 <aotonnnnn@gmail.com>
2025-05-20 23:39:19 +0900

    WaveformViewModel クラスを追加
    `WaveformViewModel.cs` に `WaveformViewModel` クラスを追加しました。このクラスは、`UcGrid` と `UcChart` のインスタンスを受け取るコンストラクタを持ち、`Initialize` メソッドでそれらを初期化します。プライベートフィールド `_waveformDataList` は定義されていますが、現在は未使用です。

---
+++ WaveformSample/Managers/WaveformViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveformSample.Charts;
+using WaveformSample.UserControls;
+using WaveformSample.Waveforms;
+
+namespace WaveformSample.Managers
+{
+    public class WaveformViewModel
+    {
+        private ObservableCollection<WaveformStep> _waveformDataList;
+        private readonly UcGrid _grid;
+        private readonly UcChart _chart;
+
+        public WaveformViewModel(UcGrid grid, UcChart chart)
+        {
+            _chart = chart;
+            _grid = grid;
+        }
+
+        public void Initialize()
+        {
+            _grid.Initialize();
+            _chart.Initialize();
+        }
+
+           
+    }
+}
